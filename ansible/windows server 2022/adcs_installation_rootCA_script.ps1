e7c26964b64f22bb4a12f6c061d960f7f4bf7d7b
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-10 12:57:23 +0200

    fix(adcs_installation_rootCA_script.ps1): fix size of script by focusing only on ADCS

--- ansible/windows server 2022/adcs_installation_rootCA_script.ps1
+++ ansible/windows server 2022/adcs_installation_rootCA_script.ps1
@@ -1,27 +1,8 @@
-# make sure to edit these values before launching the script!
-
-$root_ca_ip = "192.168.1.1"
-$intermediate_ca_ip = "192.168.1.2"
-
 # bypass Windows Powershell security controls
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-# Install the OpenSSH Server
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-
-# Start the sshd service
-Start-Service sshd
-
-# OPTIONAL but recommended:
-Set-Service -Name sshd -StartupType 'Automatic'
-
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-} else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
-}
+### make sure to edit these values before launching the script!
+$intermediate_ca_ip = "192.168.1.2"
 
 ### Installing AD CS for the PKI
 # adding windows' features
@@ -45,4 +26,4 @@ Add-CAAuthorityInformationAccess -AddToCertificateAia -Uri "http://$intermediate
 Add-CACRLDistributionPoint -AddToCertificateCdp -AddToFreshestCrl -Uri "http://$intermediate_ca_ip/certdata/<CaName><CRLNameSuffix><DeltaCRLAllowed>.crl"
 
 # publish CRL
-Certutil -CRL
+CertUtil -CRL
