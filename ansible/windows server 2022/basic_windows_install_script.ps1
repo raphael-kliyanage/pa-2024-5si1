54edf0222be7b2372e2e2774952c231144457ad0
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-10 12:58:56 +0200

    feat(basic_windows_install_script.ps1): add ip configuration pc renaming and openSSH server installation script

---
+++ ansible/windows server 2022/basic_windows_install_script.ps1
@@ -0,0 +1,38 @@
+### Edit these values to match your desired configuration
+$computer_name = "ROOT-CA"
+$ip_addr = "192.168.1.1"
+$cidr = 24
+$gateway = "192.168.1.1"
+$dns = "1.1.1.1,1.0.0.1"
+
+### IP configuration
+# get interface name
+$interface_name = (Get-NetAdapter).InterfaceAlias
+
+# configuring IPv4
+New-NetIPAddress -InterfaceAlias $interface_name -AddressFamily IPv4 -IPAddress $ip_addr -PrefixLength $cidr -DefaultGateway $gateway -Verbose
+# configuring DNS for IPv4
+Set-DnsClientServerAddress -InterfaceAlias $interface_name -ServerAddresses $dns
+
+# IPv6
+Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6
+
+### Install the OpenSSH Server for administration purposes
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+# Renaming PC
+Rename-Computer -ComputerName $env:COMPUTERNAME -NewName -Restart
