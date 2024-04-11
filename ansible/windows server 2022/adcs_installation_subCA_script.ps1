2b7e0f5052e18846ccd8e6c3c9d8d58249118db4
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-11 11:32:00 +0200

    feat(adcs subCA): add automated installation script for adcs (Enterprise subordinate CA)

---
+++ ansible/windows server 2022/adcs_installation_subCA_script.ps1
@@ -0,0 +1,22 @@
+# bypass Windows Powershell security controls
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# get current path
+$current_path = $pwd | Select -ExpandProperty Path
+
+### Installing AD CS for the PKI
+# adding windows' features
+Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools -Confirm:$false
+
+# configuring as EnterpriseSubordinateCa
+$params = @{
+    CAType              = "EnterpriseSubordinateCa"
+    CryptoProviderName  = "RSA#Microsoft Software Key Storage Provider"
+    KeyLength           = 4096
+    HashAlgorithmName   = "SHA512"
+    ValidityPeriod      = "Years"
+    ValidityPeriodUnits = 3
+}
+Install-AdcsCertificationAuthority @params -Confirm:$false
+
+Install-AdcsWebEnrollment -Confirm:$false #-Force
