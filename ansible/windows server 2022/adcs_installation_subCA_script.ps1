c957e7de846922f3c560b583559eff4248763625
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-11 16:36:22 +0200

    feat(adcs subCA): add remote files transfers via scp

--- ansible/windows server 2022/adcs_installation_subCA_script.ps1
+++ ansible/windows server 2022/adcs_installation_subCA_script.ps1
@@ -1,6 +1,11 @@
 # bypass Windows Powershell security controls
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+### Please edit these values before executing the script
+$root_ca_ip = "192.168.1.44"
+$root_ca_username = "Administrateur"
+$root_ca_computer_name = "ROOT-CA"
+
 # get current path
 $current_path = $pwd | Select -ExpandProperty Path
 
@@ -14,3 +19,13 @@ Install-AdcsCertificationAuthority -CAType EnterpriseSubordinateCa -KeyLength 40
 
 # Install Certification Authority Web Enrollment
 Install-AdcsWebEnrollment -Force
+
+### Transferring the certificates remotely
+# Revocation list
+$cert_file_name = $root_ca_computer_name
+$cert_file_name += "_$root_ca_computer_name"
+scp -r "$root_ca_computer_name\$root_ca_username@$root_ca_ip:C:\Windows\System32\CertSrv\CertEnroll\$cert_file_name-CA.crt" C:\$root_ca_username\Downloads\
+# Root certificate
+scp -r "$root_ca_computer_name\$root_ca_username@$root_ca_ip:C:\Windows\System32\CertSrv\CertEnroll\$root_ca_computer_name-CA.crl" C:\$root_ca_username\Downloads\
+
+scp -r "$root_ca_computer_name\$root_ca_username@$root_ca_ip:C:\Users\$root_ca_username\Downloads\root-ca_public_key.cer" C:\$root_ca_username\Downloads\
