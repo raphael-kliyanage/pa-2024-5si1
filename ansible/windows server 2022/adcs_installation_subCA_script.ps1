b4fe9ca18369d4d5ac053513fb08f906eb1b9820
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-11 13:06:15 +0200

    fix(adcs subCA): fix installation bugs for subornitate enterprise CA

--- ansible/windows server 2022/adcs_installation_subCA_script.ps1
+++ ansible/windows server 2022/adcs_installation_subCA_script.ps1
@@ -10,15 +10,7 @@ Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools -Confirm:$false
 Add-WindowsFeature Adcs-Web-Enrollment -Confirm:$false
 
 # configuring as EnterpriseSubordinateCa
-$params = @{
-    CAType              = "EnterpriseSubordinateCa"
-    CryptoProviderName  = "RSA#Microsoft Software Key Storage Provider"
-    KeyLength           = 4096
-    HashAlgorithmName   = "SHA512"
-    ValidityPeriod      = "Years"
-    ValidityPeriodUnits = 3
-}
-Install-AdcsCertificationAuthority @params -Confirm:$false
+Install-AdcsCertificationAuthority -CAType EnterpriseSubordinateCa -KeyLength 4096 -Confirm:$false
 
 # Install Certification Authority Web Enrollment
-Install-AdcsWebEnrollment -Confirm:$false #-Force
+Install-AdcsWebEnrollment -Force
