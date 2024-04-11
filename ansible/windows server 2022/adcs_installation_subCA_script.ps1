d0ce3c9997faa3d0a65fd6080813173d06473cfe
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-11 12:18:44 +0200

    fix(adcs subCA): fix installation bug for Certification Authority Web Enrollment

--- ansible/windows server 2022/adcs_installation_subCA_script.ps1
+++ ansible/windows server 2022/adcs_installation_subCA_script.ps1
@@ -7,6 +7,7 @@ $current_path = $pwd | Select -ExpandProperty Path
 ### Installing AD CS for the PKI
 # adding windows' features
 Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools -Confirm:$false
+Add-WindowsFeature Adcs-Web-Enrollment -Confirm:$false
 
 # configuring as EnterpriseSubordinateCa
 $params = @{
@@ -19,4 +20,5 @@ $params = @{
 }
 Install-AdcsCertificationAuthority @params -Confirm:$false
 
+# Install Certification Authority Web Enrollment
 Install-AdcsWebEnrollment -Confirm:$false #-Force
