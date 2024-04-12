5a020f68429bc7b1eeda4c31de6a68e0dfc1c8f2
Mathis <mathis.thouvenin@orange.fr>
2024-04-12 15:45:19 +0200

    French to English

--- ansible/windows server 2022/Script_Installation_AD_DNS.ps1
+++ ansible/windows server 2022/Script_Installation_AD_DNS.ps1
@@ -3,28 +3,29 @@
 # Param 1       :
 # Param 2       :
 #
-# Exemple       : ./Script_AD_DNS.ps1
+# Exemple       : ./Script_Installation_AD_DNS.ps1
 #
-# Auteur        : Mathis THOUVENIN, Raphaël KATHALUWA-LIYANAGE, Lyronn LEVY
+# Author        : Mathis THOUVENIN, Raphaël KATHALUWA-LIYANAGE, Lyronn LEVY
 # Changelog     :
-# Version       : 0.3
+# Version       : 0.5
 #
 #
 
-# Paramètres
+# Parameters
 $domainName = "quinteflush.org"
 $domainNetBIOSName = "QUINTEFLUSH"
 $domainAdminPassword = ConvertTo-SecureString "changeme123." -AsPlainText -Force
 $mode = "WinThreshold"
-# Installer les rôles AD DS et DNS
+
+# Install AD DS and DNS roles
 Install-WindowsFeature -Name AD-Domain-Services,DNS -IncludeManagementTools
 
-# Promotion du serveur en contrôleur de domaine
+# Promoting the server to domain controller
 Install-ADDSForest -DomainName $domainName -DomainNetBIOSName $domainNetBIOSName -ForestMode $mode -DomainMode $mode -SafeModeAdministratorPassword $domainAdminPassword -Force:$true -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -NoRebootOnCompletion:$false
 
-# Configuration des paramètres DNS
+# Configuring DNS parameters
 $dnsServerAddress = "127.0.0.1"
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServerAddress
 
-# Redémarrer le serveur pour appliquer les modifications
+# Restart the server to apply the changes
 Restart-Computer -Force
