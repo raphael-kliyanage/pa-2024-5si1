6574a32185511c3fa7cf1f3611ec12ef424749fb
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-04-21 11:59:49 +0200

    feat(adds gpo): add gpo-hardening script

---
+++ ansible/windows server 2022/adds_hardening-gpo.ps1
@@ -0,0 +1,32 @@
+### Please edit these values before executing the script
+# guide: https://www.youtube.com/watch?v=7hoQcEqIUYE
+
+$main_dc_ip = "192.168.1.60"
+$ad_domain = "quinteflush.org"
+
+# importing AD Powershell module
+Import-module ActiveDirectory
+# storing default policies Id to edit them
+$domain_policy = Get-GPO "Default Domain Policy" | Select-Object -Expand Id
+$dc_policy = Get-GPO "Default Domain Controllers Policy" | Select-Object -Expand Id
+
+### Privileged Accounts hardening - ANSSI
+# enabling recycle bin
+Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target $ad_domain
+
+# Getting all the users with "Schema Admins" right roles
+Get-ADGroupMember -Identity "Schema Admins" | Select-Object Name, SamAccountName
+ 
+#Remove users from Groups
+$user_rights = "Schema Admins"
+foreach ($access_right in $user_rights) {
+    Get-ADGroupMember -Identity $access_right | %{Remove-ADGroupMember -Identity $access_right -Member $_ -Confirm:$false}
+}
+
+# Protecting all OUs from accidental deletion
+Get-ADOrganizationalUnit -filter {name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
+
+### Trusts harding
+# for a future release
+
+### Anomalies analysis remediation/hardening
