3f78f9b85d21a3a3562b076ffb9202cfb1f9e4db
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-05-01 10:15:56 +0200

    feat(gpo): add gpo importation script

--- ansible/windows server 2022/adds_hardening-gpo.ps1
+++ ansible/windows server 2022/adds_hardening-gpo.ps1
@@ -4,26 +4,31 @@
 $main_dc_ip = "192.168.1.60"
 $ad_domain = "quinteflush.org"
 
+## Importing new GPOs to replace current ones
+Write-Host "Importing GPOs..."
+Import-GPO -BackupGpoName 'Default Domain Policy' -TargetName 'Default Domain Policy' -path '.\gpo'
+Import-GPO -BackupGpoName 'Default Domain Controllers Policy' -TargetName 'Default Domain Controllers Policy' -path '.\gpo'
+
 # importing AD Powershell module
 Import-module ActiveDirectory
 # storing default policies Id to edit them
-$domain_policy = Get-GPO "Default Domain Policy" | Select-Object -Expand Id
-$dc_policy = Get-GPO "Default Domain Controllers Policy" | Select-Object -Expand Id
+#$domain_policy = Get-GPO "Default Domain Policy" | Select-Object -Expand Id
+#$dc_policy = Get-GPO "Default Domain Controllers Policy" | Select-Object -Expand Id
 
 ### Privileged Accounts hardening - ANSSI
 # enabling recycle bin
 Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target $ad_domain
 
 # Getting all the users with "Schema Admins" right roles
-Get-ADGroupMember -Identity "Schema Admins" | Select-Object Name, SamAccountName
+#Get-ADGroupMember -Identity "Schema Admins" | Select-Object Name, SamAccountName
  
 #Remove users from Groups
 Write-Host "Emptying `"Schema Admins`" rights..."
-$user_rights = "Schema Admins"
-foreach ($access_right in $user_rights) {
-    Get-ADGroupMember -Identity $access_right | %{Remove-ADGroupMember -Identity $access_right -Member $_ -Confirm:$false}
-}
+#$user_rights = "Schema Admins"
+#foreach ($access_right in $user_rights) {
+#    Get-ADGroupMember -Identity $access_right | %{Remove-ADGroupMember -Identity $access_right -Member $_ -Confirm:$false}
+#}
 
 # Protecting all OUs from accidental deletion
 Write-Host "Protecting all OUs from accidental deletion..."
-Get-ADOrganizationalUnit -filter {name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
+#Get-ADOrganizationalUnit -filter {name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
