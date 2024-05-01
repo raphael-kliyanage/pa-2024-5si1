a51678c788d9b2e896053813499ced0952622e8e
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-05-01 12:17:37 +0200

    feat(gpo): add user friendly prompt messages and adjusted codes and comments

--- ansible/windows server 2022/adds_hardening-gpo.ps1
+++ ansible/windows server 2022/adds_hardening-gpo.ps1
@@ -1,34 +1,31 @@
 ### Please edit these values before executing the script
 # guide: https://www.youtube.com/watch?v=7hoQcEqIUYE
 
-$main_dc_ip = "192.168.1.60"
 $ad_domain = "quinteflush.org"
 
+# importing AD Powershell module to use AD commands via powershell
+Import-module ActiveDirectory
+
 ## Importing new GPOs to replace current ones
+# These GPOs are the default ones provided by ADDS. They have been slightly updated (e.g. password policy, audit policy, NETLOGON & SYSVOL hardening...)
+# to be slightly more compliant with ANSSI's recommendations (i.e. PingCastle). They are export in xml in the github repository.
 Write-Host "Importing GPOs..."
 Import-GPO -BackupGpoName 'Default Domain Policy' -TargetName 'Default Domain Policy' -path '.\gpo' -CreateIfNeeded:$true -Confirm:$false
 Import-GPO -BackupGpoName 'Default Domain Controllers Policy' -TargetName 'Default Domain Controllers Policy' -path '.\gpo' -CreateIfNeeded:$true -Confirm:$false
 
-# importing AD Powershell module
-Import-module ActiveDirectory
-# storing default policies Id to edit them
-#$domain_policy = Get-GPO "Default Domain Policy" | Select-Object -Expand Id
-#$dc_policy = Get-GPO "Default Domain Controllers Policy" | Select-Object -Expand Id
-
 ### Privileged Accounts hardening - ANSSI
-# enabling recycle bin
+# enabling recycle bin to recover ADDS objects if deleted by accident
+Write-Host "Enabling Recycle Bin..."
 Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target $ad_domain -Confirm:$false
 
-# Getting all the users with "Schema Admins" right roles
-#Get-ADGroupMember -Identity "Schema Admins" | Select-Object Name, SamAccountName
- 
-#Remove users from Groups
-Write-Host "Emptying `"Schema Admins`" rights..."
-#$user_rights = "Schema Admins"
-#foreach ($access_right in $user_rights) {
-#    Get-ADGroupMember -Identity $access_right | %{Remove-ADGroupMember -Identity $access_right -Member $_ -Confirm:$false}
-#}
-
 # Protecting all OUs from accidental deletion
+# Enabling this features requires users to make several manipulations to delete an OU.
 Write-Host "Protecting all OUs from accidental deletion..."
-#Get-ADOrganizationalUnit -filter {name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
+Get-ADOrganizationalUnit -filter {name -like "*"} -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
+
+# Applying the new GPOs
+gpupdate /force
+
+# User friendly ending of the program
+Write-Host "Hardening done! Press any keys to continue..." -ForegroundColor Black -BackgroundColor White
+$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
