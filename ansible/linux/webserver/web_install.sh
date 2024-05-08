9ec7cb906ce61fc9273585a8870fbdff7fc6d3c8
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-05-08 17:58:06 +0200

    feat(linux webserver): add alpha v.0.1.0 of web server automation

---
+++ ansible/linux/webserver/web_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+$dns1 = "192.168.1.53"
+$dns2 = "1.1.1.1"
+
+### configure ip address
+# backing up original configuration file for network
+mv /etc/network/interfaces /etc/network/interfaces.bak
+# writing the new ip configuration file, to a static ipv4 address
+cp configuration_files/interfaces /etc/network/interfaces
+
+# backing up original DNS server
+mv /etc/resolv.conf /etc/resolv.conf.bak
+# writing new DNS servers
+echo "nameserver $dns1" > /etc/resolv.conf # overwriting previous DNS servers
+echo "nameserver $dns2" >> /etc/resolv.conf # add dns 2 at eof without overwriting
+
+# updating system packages
+apt update && apt dist-upgrade -y
