ea5df0ecfc86620f4ed5017575058f2dc74fd4c7
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-05-25 14:44:05 +0200

    fix: add access to wordpress to access database

--- ansible/linux/db_install.sh
+++ ansible/linux/db_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ### please modify the following parameters before launching
 # the script
-ip="192.168.1.98"
+ip_local_server="192.168.1.98"
+ip_remote_wordpress="192.168.1.97"
 cidr="24"
 gateway="192.168.1.1"
 dns1="192.168.1.53"
@@ -15,7 +16,7 @@ cp /etc/network/interfaces /etc/network/interfaces.bak
 echo "Configuring static ip address..."
 sed -i "s/dhcp/static/g" /etc/network/interfaces
 cat << EOF | tee -a /etc/network/interfaces
-  address $ip/$cidr
+  address $ip_local_server/$cidr
   gateway $gateway
 EOF
 # backing up original DNS server
@@ -41,13 +42,13 @@ read -p "Choose your wordpress database password:   " db_passwd
 mariadb -u root -e "
     CREATE DATABASE wordpress_db DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
     GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@localhost IDENTIFIED BY '$db_passwd';
-    GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$ip IDENTIFIED BY '$db_passwd';
+    GRANT ALL PRIVILEGES ON wordpress_db.* TO wordpress_user@$ip_remote_wordpress IDENTIFIED BY '$db_passwd';
     FLUSH PRIVILEGES;"
 
 mysql_secure_installation
 
 # accept remote connection
-sed -i "s/127.0.0.1/$ip/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/127.0.0.1/$ip_local_server/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 systemctl restart mariadb
 systemctl enable mariadb
 
