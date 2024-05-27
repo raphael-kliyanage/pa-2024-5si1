aab01320e838a838cbf7ab8de044ccb94b8e4a2b
Raphaël LIYANAGE <raphael.kliyanage@gmail.com>
2024-05-27 18:57:09 +0200

    test: add tls support on wordpress

--- ansible/linux/web_install.sh
+++ ansible/linux/web_install.sh
@@ -7,6 +7,10 @@ cidr="24"
 gateway="192.168.1.1"
 dns1="192.168.1.53"
 dns2="1.1.1.1"
+# variables concerning the pki
+root_ca_ip="192.168.1.44"
+root_ca_username="Administrateur"
+root_ca_computer_name="SRV-WIN-ROOT"
 
 ### configure ip address
 # backing up original configuration file for network
@@ -47,8 +51,69 @@ chown -R www-data:www-data wordpress
 rm latest.zip
 mv wordpress /var/www
 
+# creating directory to store cryptographic keys for wordpress
+mkdir /etc/apache2/tls/
+
+# creating 
+cat << EOF | tee -a /etc/apache2/tls/certificate_request.conf
+# TLS server certificate request
+
+# This file is used by the openssl req command. The subjectAltName cannot be
+# prompted for and must be specified in the SAN environment variable.
+
+[ default ]
+SAN                     = DNS:quinteflush.org    # Default value
+
+[ req ]
+default_bits            = 3072                  # RSA key size
+encrypt_key             = no                    # Protect private key
+default_md              = sha512                # MD to use
+utf8                    = yes                   # Input is UTF-8
+string_mask             = utf8only              # Emit UTF-8 strings
+prompt                  = yes                   # Prompt for DN
+distinguished_name      = server_dn             # DN template
+req_extensions          = server_reqext         # Desired extensions
+
+[ server_dn ]
+0.domainComponent       = "1. Domain Component         (eg, com)      "
+1.domainComponent       = "2. Domain Component         (eg, company)  "
+# 2.domainComponent       = "3. Domain Component         (eg, pki)      "
+organizationName        = "4. Organization Name        (eg, company)  "
+organizationalUnitName  = "5. Organizational Unit Name (eg, section)  "
+commonName              = "6. Common Name              (eg, FQDN)     "
+commonName_max          = 64
+
+[ server_reqext ]
+keyUsage                = critical,digitalSignature,keyEncipherment
+extendedKeyUsage        = serverAuth,clientAuth
+subjectKeyIdentifier    = hash
+subjectAltName          = $ENV::SAN
+EOF
+
+# creating certificate request
+SAN=DNS:www.quinteflush.org \
+	openssl req -new \
+	-config "${server_conf}" \
+	-out /etc/apache2/tls/wordpress.csr \
+	-keyout /etc/apache2/tls/wordpress.key
+
+# uploading the certificate request via scp to the subordinate ca
+scp -r /etc/apache2/tls/wordpress.csr "$root_ca_computer_name\\$root_ca_username@$root_ca_ip:C:\Users\\$root_ca_username\Downloads\root-ca_public_key.cer"
+
+# storing signed certificate
+echo "Rename the signed certificate \"wordpress.crt\" then"
+read -p "press enter to continue"
+
+# enable tls on apache2
+# https://gist.github.com/kjohnson/68959c8615e0205f48adefcce9e65645
 cat << EOF | tee -a /etc/apache2/sites-available/wordpress.conf
-<VirtualHost *:80>
+<VirtualHost *:443>
+    SSLEngine On
+    SSLCertificateFile /etc/apache2/tls/wordpress.crt
+    SSLCertificateKeyFile /etc/apache2/tls/wordpress.key
+    ServerName quinteflush.org
+    ServerAlias www.quinteflush.org
+    Redirect permanent / https://quinteflush.org/
     DocumentRoot /var/www/wordpress
     <Directory /var/www/wordpress>
         Options FollowSymLinks
@@ -90,7 +155,3 @@ echo "Database Name:    \"wordpress_db\""
 echo "Password:         mariadb_password"
 echo "Database Host:    mariadb_ip"
 echo "Table Prefix:     \"wp_\""
-
-# todo
-# add certificates
-# enable tls 1.3
