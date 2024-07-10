5af833443c096dec729a2a98d08b95ff2689ef40
Mathis <mathis.thouvenin@orange.fr>
2024-07-10 10:56:50 +0200

    Rename DC2 to DC2_Agent_Wazuh wiht add install Agent Wazuh

--- ansible/windows server 2022/Script_Install_DC2_Agent_Wazuh.ps1
+++ ansible/windows server 2022/Script_Install_DC2_Agent_Wazuh.ps1
@@ -1,13 +1,13 @@
-# Name          : Script_Install_DC2.ps1
+# Name          : Script_Install_DC2_Agent_Wazuh.ps1
 # Description   : Install Domain Controller 2
 # Param 1       :
 # Param 2       :
 #
-# Exemple       : ./Script_Install_DC2.ps1
+# Exemple       : ./Script_Install_DC2_Agent_Wazuh.ps1
 #
 # Author        : Mathis THOUVENIN, Raphaël KATHALUWA-LIYANAGE, Lyronn LEVY
 # Changelog     :
-# Version       : 0.6
+# Version       : 0.8
 #
 #
 
@@ -15,6 +15,12 @@
 $domainName = "quinteflush.org"
 $domainAdmin = "Administrateur"
 $adminPassword = Read-Host -Prompt "Entrez le mot de passe de l'administrateur du domaine" -AsSecureString
+$ipmanager = "10.0.0.2"
+$agentname = "DC2-AD-Bruh2"
+$agentgroup = "windows"
+
+# Install Agent Wazuh (We need to have msi agent wazuh before you execute the script)
+.\wazuh-agent-4.8.0-1.msi /q WAZUH_MANAGER=$ipmanager WAZUH_AGENT_NAME=$agentname WAZUH_AGENT_GROUP=$agentgroup
 
 # Name of the existing domain controller
 $existingDC = "AD-BRUH2.quinteflush.org" 
@@ -30,8 +36,6 @@ Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $dnsServe
 
 # Join the existing domain
 Add-Computer -DomainName $domainName -Credential $domainAdmin -Restart
-
-Start-Sleep -Seconds 30
 
 # Promoting the server to domain controller
 Install-ADDSDomainController -DomainName $domainName -Credential (New-Object System.Management.Automation.PSCredential($domainAdmin, $adminPassword)) -InstallDns -NoGlobalCatalog:$false -SiteName "Default-First-Site-Name" -Force:$true -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -NoRebootOnCompletion -ReplicationSourceDC $existingDC
